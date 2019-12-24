a06aa1200b9b1dfeb80fb8819f72cbbb932c0d97
Greg Pevnev <grigory.pevnev@serokell.io>
2019-12-24 10:29:16 +0300

    fixup! [SDAO-30] Add `EnsureOwner` newtype for STKR

--- stkr-token/src/Lorentz/Contracts/STKR/Misc/TypeDefs.hs
+++ stkr-token/src/Lorentz/Contracts/STKR/Misc/TypeDefs.hs
@@ -2,15 +2,17 @@ module Lorentz.Contracts.STKR.Misc.TypeDefs
   ( EnsureOwner(..)
   ) where
 
+import Prelude
 import Lorentz
 
-newtype EnsureOwner a = EnsureOwner a
+newtype EnsureOwner a = EnsureOwner { unEnsureOwner :: a }
   deriving stock Generic
   deriving anyclass IsoValue
 
+-- TODO: Resulting doc render not tested. Take care of this when autodoc is ready.
 instance (TypeHasDoc a) => TypeHasDoc (EnsureOwner a) where
-  typeDocMdDescription = ""
-  typeDocMdReference = undefined
-  typeDocDependencies = undefined
-  typeDocHaskellRep = undefined
-  typeDocMichelsonRep = undefined
+  typeDocMdDescription = "This type reprsents requirires that this entrypoint caller must be owner."
+  typeDocMdReference = poly1TypeDocMdReference
+  typeDocDependencies p = genericTypeDocDependencies p <> [SomeTypeWithDoc (Proxy @a)]
+  typeDocHaskellRep proxy = typeDocHaskellRep (unEnsureOwner <$> proxy)
+  typeDocMichelsonRep proxy = typeDocMichelsonRep (unEnsureOwner <$> proxy)

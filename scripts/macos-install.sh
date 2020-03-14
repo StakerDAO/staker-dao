101cceabb1d8597557945629e4b85a5e214c7e0e
Greg Pevnev <grigory.pevnev@serokell.io>
2020-03-14 22:00:01 +0300

    [SDAO-95] Add stetup script for macOS using brew

---
+++ scripts/macos-install.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+tezos_client_version="2020-03-11-ledger-patched"
+
+brew tap gpevnev/stakerdao
+brew install gpevnev/stakerdao/tezos-client-patched
+
+tezos_client="$(brew --cellar tezos)/$tezos_client_version/bin/tezos-client"
+
+echo $tezos_client
+
+brew install gpevnev/stakerdao/stkr-token-cli
+
+config_dir="$HOME/stkr-token-cli"
+config_file="$config_dir/config.yaml"
+
+mkdir -p config_dir
+
+rm $config_file
+
+echo "envTezosClientCmd: \"$tezos_client\"" >> $config_file
+echo 'envNodeAddr:' >> $config_file
+echo '    naHost: "carthage.testnet.tezos.serokell.team"' >> $config_file
+echo '    naPort: 8732' >> $config_file

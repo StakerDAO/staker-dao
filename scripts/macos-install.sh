7c30d49f1ba7cdd790fb2a1659893274cc18612d
Greg Pevnev <grigory.pevnev@serokell.io>
2020-03-26 21:58:41 +0300

    fixup! [SDAO-95] Add stetup script for macOS using brew

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -1,13 +1,11 @@
 #! /usr/bin/env bash
 tezos_client_version="2020-03-11-ledger-patched"
-config_template_link="https://raw.githubusercontent.com/StakerDAO/staker-dao/master/stkr-token/tezos-nodes/serokell.yaml"
+config_template_link="https://raw.githubusercontent.com/StakerDAO/staker-dao/d9b84eecd013970c697392cae6bece750f22d117/scripts/config-template.yaml"
 
 brew tap gpevnev/stakerdao
 brew install gpevnev/stakerdao/tezos-client-patched
 
 tezos_client="$(brew --cellar gpevnev/stakerdao/tezos-client-patched)/$tezos_client_version/bin/tezos-client"
-
-echo $tezos_client
 
 brew install gpevnev/stakerdao/stkr-token-cli
 
